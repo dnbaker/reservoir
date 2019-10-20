f223e891edb86e359b239a66edf17963b04cb8c0
dnbaker <dnb@cs.jhu.edu>
2019-10-20 11:15:19 -0400

    Small tweaks.

--- README.md
+++ README.md
@@ -22,9 +22,16 @@ Template arguments are:
 1. `value type`
     1. Note: these are added by rvalue reference, so they work for any RAII type.
 2. `RNG`
-    1. Defaults to `std::mt19937_64`, which is rather slow
-    2. seed with `reservoir.seed(newseed)`
+    1. Defaults to WyRand.
+    2. seed with `reservoir.seed(newseed)`.
+3. `Container`
+    1. Defaults to std::vector.
+    2. for advanced usage only
+    2. must be a template with `value_type` as the first parameter.
+    4. Must support `std::begin`
+    5. If it doesn't support `emplace_back`, specialize `Emplacer` for your type.
+    6. Non-member function `size` is used to assess the size of the container.
+        1. Defaults to `std::size`.
+        2. If no member function or overload is available, provide one.
 3.  `VectorArgs...` 
     1. Defaults to nothing, but can be used to provide additional template arguments to the container.
-
-We may allow templating by container template additionally.
--- include/reservoir.h
+++ include/reservoir.h
@@ -6,66 +6,70 @@
 #include <cstdlib>
 #include <cassert>
 #include "./div.h"
+#include "aesctr/wy.h"
 
 namespace reservoir_dogs {
 using std::size_t;
 
 template<typename C>
 struct Emplacer {
-    template<typename Item>
-    static void insert_new(C &c, Item &&item) {
-        c.emplace_back(std::move(item));
+    static void reserve(C &c, size_t n) {
+        c.reserve(n);
     }
     template<typename Item>
-    static void insert_replace(Item &&item, Item &to_replace) {
-        to_replace = std::move(item);
+    static void insert(C &c, Item &&item) {
+        c.emplace_back(std::move(item));
     }
 };
 
 
 template<typename T>
-struct pass_by_val_faster: public std::integral_constant<bool, sizeof(T) > 16 || !std::is_trivially_copyable_v<T>> {};
+struct pass_by_val_faster: public std::integral_constant<bool, (sizeof(T) & 15 == 0) || !std::is_trivially_copyable_v<T>> {};
 template<typename T>
 bool pass_by_val_faster_v = pass_by_val_faster<T>::value;
 
-template<typename T, typename RNG=std::mt19937_64,
+template<typename T, typename RNG=wy::WyHash<uint32_t, 16>,
+         template<typename...> class Container=std::vector,
          typename...VectorTemplateArgs>
 class Reservoir {
     using rng_type = typename RNG::result_type;
+    using CType = Container<T, VectorTemplateArgs...>;
 
     size_t n_;
     size_t t_;
-    using CType = std::vector<T, VectorTemplateArgs...>;
     CType v_;
     RNG rng_;
     schism::Schismatic<uint32_t> div_;
 
 public:
     Reservoir(size_t n): n_(n), t_(0), div_(n) {
-        v_.reserve(n);
+        Emplacer<CType>::reserve(v_, n);
     }
     template<typename...Args>
     Reservoir(size_t n, Args &&...args): n_(n), t_(0), v_(std::forward<Args>(args)...), div_(n) {
-        v_.reserve(n);
+        Emplacer<CType>::reserve(v_, n);
     }
     void seed(uint64_t s) {rng_.seed(s);}
     bool add(T &&x) {
         static constexpr double dt = double(std::numeric_limits<rng_type>::max());
         bool ret = true;
         if(size() < n()) {
-            Emplacer<CType>::insert_new(v_, std::move(x));
+            Emplacer<CType>::insert(v_, std::move(x));
             if(v_.size() == n())
                 t_ = n_;
         } else if (rng_() < (dt * (double(n_) / ++t_))) {
             auto it = std::begin(v_);
             std::advance(it,  div_.mod(rng_()));
-            Emplacer<CType>::insert_replace(std::move(x), *it);
+            *it = std::move(x);
         } else ret = false;
         return ret;
     }
-    size_t size() const {return v_.size();}
+    size_t size() const {
+        using std::size;
+        return size(v_);
+    }
     size_t n()    const {return n_;}
-    bool full() const {return n_ == v_.size();}
+    bool full() const {return n_ == size();}
     CType       &container()       {return v_;}
     const CType &container() const {return v_;}
 };  
