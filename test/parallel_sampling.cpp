d789e4bafd85d43d62adbf0e9d1acfc14183f229
dnbaker <dnb@cs.jhu.edu>
2020-10-01 10:52:45 -0400

    Tweak

--- test/parallel_sampling.cpp
+++ test/parallel_sampling.cpp
@@ -3,7 +3,7 @@
 int main() {
     std::vector<double> weights;
     while(weights.size() < 100) weights.emplace_back(double(std::rand() ) / RAND_MAX * 1e6);
-    while(weights.size() < 10000000) weights.emplace_back(double(std::rand() ) / RAND_MAX);
+    while(weights.size() < 100000000) weights.emplace_back(double(std::rand() ) / RAND_MAX);
     weights.emplace_back(1e10);
     std::vector<uint32_t> ind(weights.size());
     std::iota(ind.begin(), ind.end(), 0u);
@@ -25,5 +25,5 @@ int main() {
     tmp.add(ind.begin(), ind.end(), weights.data());
     auto t6 = std::chrono::high_resolution_clock::now();
     long long unsigned d1 = (t2 - t).count(), d2 = (t4 - t3).count(), d3 = (t6 - t5).count();
-    std::fprintf(stderr, "First two tries: %llu/%llu. One thread: %llu. Ratio: %g\n", d1, d2, d3, double(d3 / std::max(d1, d2)));
+    std::fprintf(stderr, "First two tries: %llu/%llu. One thread: %llu. Ratio: %g\n", d1, d2, d3, double(d3) / std::max(d1, d2));
 }
