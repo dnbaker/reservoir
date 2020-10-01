93c2f179dc3a38338bb9a3af8bc4a529cf6b9f66
dnbaker <dnb@cs.jhu.edu>
2020-09-30 23:48:48 -0400

    Tweaks to Calaveras sampler

--- include/DOGS/reservoir.h
+++ include/DOGS/reservoir.h
@@ -79,11 +79,16 @@ public:
 };
 
 template<typename T, typename RNG=wy::WyRand<uint32_t>,
-         template<typename...> class Container=std::vector,
          typename IT=uint32_t,
          typename...VectorTemplateArgs>
 class CalaverasReservoirSampler {
-    using CType = std::priority_queue<std::pair<double, T>, Container<VectorTemplateArgs...>, std::greater<>>;
+    using base_type = std::priority_queue<std::pair<double, T>, std::vector<std::pair<double, T>, VectorTemplateArgs...>, std::greater<>>;
+    struct CType: public base_type {
+        //template<typename...Args>
+        //CType(Args &&...args): base_type(std::forward<Args>(args)...) {}
+        auto &getc() {return this->c;}
+        const auto &getc() const {return this->c;}
+    };
     using rng_type = typename RNG::result_type;
 
     double x_;
@@ -96,14 +101,43 @@ class CalaverasReservoirSampler {
     static_assert(sizeof(rng_type) == sizeof(IT), "Ensure RNG type and integral type are of the same size");
 public:
     CalaverasReservoirSampler(size_t n): n_(n), t_(0), div_(n) {
-        Emplacer<CType>::reserve(v_, n);
+        v_.getc().reserve(n);
     }
     template<typename...Args>
     CalaverasReservoirSampler(size_t n, Args &&...args): n_(n), t_(0), v_(std::forward<Args>(args)...), div_(n) {
-        Emplacer<CType>::reserve(v_, n);
+        v_.getc().reserve(n);
     }
     void seed(uint64_t s) {rng_.seed(s);}
-    bool add(T &&x, double weight=1.) {
+    template<typename IT1, typename IT2, typename WIT>
+    void add(IT1 beg, IT2 end, WIT wbeg) {
+        while(beg != end) add(*beg++, *wbeg++);
+    }
+    template<typename IT1, typename IT2>
+    void add(IT1 beg, IT2 end) {
+        while(beg != end) add(*beg++);
+    }
+    bool add(T x, double weight=1.) {
+        std::uniform_real_distribution<double> urd;
+        if(weight <= 0.) return false;
+        if(size() < n()) {
+            v_.push(std::make_pair(std::pow(urd(rng_), 1. / weight), std::move(x)));
+            if(v_.size() == n_) {
+                x_ = std::log(urd(rng_)) / std::log(v_.top().first);
+            }
+            return true;
+        } else if((x_ -= weight) <= 0.) {
+            auto t = std::pow(v_.top().first, weight);
+            v_.pop();
+            auto t1 = urd(rng_) * (1. - t) + t; // Uniform between t and 1
+            auto r = std::pow(t1, 1. / weight);
+            v_.push(std::make_pair(r, x));
+            x_ = std::log(urd(rng_)) / std::log(v_.top().first);
+            return true;
+        }
+        return false;
+    }
+#if 0
+    bool add(T &x, double weight=1.) {
         std::uniform_real_distribution<double> urd;
         if(weight <= 0.) return false;
         if(size() < n()) {
@@ -123,14 +157,15 @@ public:
         }
         return false;
     }
+#endif
     size_t size() const {
         using std::size;
         return size(v_);
     }
     size_t n()  const {return n_;}
     bool full() const {return n_ == size();}
-    CType       &container()       {return v_;}
-    const CType &container() const {return v_;}
+    auto &container()       {return v_.getc();}
+    const auto &container() const {return v_.getc();}
 };
 
 } // reservoir
