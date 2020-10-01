9a79f1cf45e78c0f54bf629c11fde4f5e5ea543e
dnbaker <dnb@cs.jhu.edu>
2020-10-01 12:07:14 -0400

    Update Travis

--- Makefile
+++ Makefile
@@ -1,16 +1,16 @@
+.PHONY=all clean
+
 
 CXX?=g++
 INCLUDE=-I. -Iinclude
 STD=-std=c++17
 FLAGS=-O3 -Wall -Wextra $(INCLUDE) $(STD) -Werror
 
-
-all: pri rsv crsv parallel_sampling
+EX=pri rsv crsv parallel_sampling
+all: $(EX)
 %: test/%.cpp
 	$(CXX) $(FLAGS) $< -o $@ && ./$@
-parallel_sampling: test/parallel_sampling.cpp
-	$(CXX) $(FLAGS) $< -o $@
 
 
 clean:
-	rm -f pri rsv
+	rm -f $(EX)
--- test/pri.cpp
+++ test/pri.cpp
@@ -6,9 +6,10 @@ int main() {
         ps.add(i, 100000u - i);
     }
     PrioritySampledSketch<int, double> pss(ps);
+    bool emitted = false;
     for(size_t i = 0; i < 100000u; ++i) {
         auto e = pss.estimate(i);
-        if(e)
-            std::fprintf(stderr, "estimate: %f\n", e);
+        if(e && !emitted)
+            std::fprintf(stderr, "estimate: %f\n", e), emitted = true;
     }
 }
